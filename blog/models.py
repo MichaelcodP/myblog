ca701ec824ed51ce52e7d62e6ac9909116fc7fb5
MichaelcodP <mishap7103@gmail.com>
2025-09-19 17:15:26 +0200

    Removed unused imports

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)  # up to 100 characters
     body = models.CharField(max_length=255)   # up to 255 characters
     author = models.ForeignKey(
-        User, 
+        User,
         on_delete=models.CASCADE  # if a user is deleted, their posts are deleted
     )
 
