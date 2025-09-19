17e584a858ed098769863543b0a1460740f3ac75
MichaelcodP <mishap7103@gmail.com>
2025-09-19 15:43:02 +0200

    Resolved conflict in models.py

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class BlogPost(models.Model):
-    title = models.CharField(max_length=100)  # до 100 символів
-    body = models.CharField(max_length=255)   # до 255 символів
+    title = models.CharField(max_length=100)  # up to 100 characters
+    body = models.CharField(max_length=255)   # up to 255 characters
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE  # якщо користувач видаляється, видаляються його пости
+        User, 
+        on_delete=models.CASCADE  # if a user is deleted, their posts are deleted
     )
 
     def __str__(self):
-        return self.title  # показуємо назву поста як display name
+        return self.title  # show the post name as display name
 
     class Meta:
         verbose_name = "Blog Post"
