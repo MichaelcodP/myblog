b7181abc379f55e0ee6e3584b9cc31aace87649b
MichaelcodP <mishap7103@gmail.com>
2025-09-28 22:42:58 +0200

    removed duplicate BlogPostViewSet from views.py

--- blog/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import BlogPost
-from .api.serializers import BlogPostSerializer
-
-
-class BlogPostViewSet(viewsets.ModelViewSet):
-    queryset = BlogPost.objects.all()
-    serializer_class = BlogPostSerializer
