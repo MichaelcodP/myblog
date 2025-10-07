87a76fcb997adef722552050fefd64f5ec63583f
MichaelcodP <mishap7103@gmail.com>
2025-10-07 17:57:17 +0200

    feat: Changed utils.py

--- blog/utils.py
+++ blog/utils.py
@@ -1,5 +1,11 @@
 import redis
+from django.conf import settings
 
 
 def get_redis_connection():
-    return redis.Redis(host="localhost", post=6379, db=0, decode_responses=True)
+    return redis.Redis(
+        host=getattr(settings, "REDIS_HOST", "localhost"),
+        port=getattr(settings, "REDIS_PORT", 6379),
+        db=getattr(settings, "REDIS_DB", 0),
+        decode_responses=True,
+    )
